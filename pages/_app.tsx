228eeeef8c760dc44aceba040f76abdd6fba1a9e
Herbie Vine <vineherbie@gmail.com>
2021-03-04 22:27:38 +0000

    tailwind not working in prod

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
-import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeContext } from '../components/ThemeContext'
 import React, { useState } from 'react'
