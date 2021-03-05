c1e98232916ec97d93027eb65ef5605149828b3f
Herbie Vine <vineherbie@gmail.com>
2021-03-05 14:52:23 +0000

    cleaned up unused imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,7 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeContext } from '../components/ThemeContext'
 import React, { useState } from 'react'
-// import { Provider, createClient, dedupExchange, fetchExchange } from 'urql'
-// import { cacheExchange } from '@urql/exchange-graphcache'
 import Navigation from '../modules/Navigation'
-// import {
-//     MeDocument,
-//     LoginMutation,
-//     RegisterMutation,
-//     MeQuery,
-//     LogoutMutation
-// } from '../generated/graphql'
-// import { updateQuery } from '../utils/urqlFunctionWrapper'
-
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -26,69 +15,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         credentials: 'include'
     });
 
-    // const client = createClient({
-    //     url: 'http://localhost:8080/graphql',
-    //     exchanges: [
-    //         dedupExchange,
-    //         fetchExchange,
-    //         cacheExchange({
-    //             updates: {
-    //                 Mutation: {
-    //                     login: (res, _, cache) => {
-    //                         updateQuery<LoginMutation, MeQuery>(
-    //                             cache,
-    //                             { query: MeDocument },
-    //                             res,
-    //                             (result, query) => {
-    //                                 if (result.login.errors) {
-    //                                     return query
-    //                                 } else {
-    //                                     return {
-    //                                         me: result.login.user
-    //                                     }
-    //                                 }
-    //                             }
-    //                         )
-    //                     },
-    //                     register: (res, _, cache) => {
-    //                         updateQuery<RegisterMutation, MeQuery>(
-    //                             cache,
-    //                             { query: MeDocument },
-    //                             res,
-    //                             (result, query) => {
-    //                                 if (result.register.errors) {
-    //                                     return query
-    //                                 } else {
-    //                                     return {
-    //                                         me: result.register.user
-    //                                     }
-    //                                 }
-    //                             }
-    //                         )
-    //                     },
-    //                     logout: (res, _, cache) => {
-    //                         updateQuery<LogoutMutation, MeQuery>(
-    //                             cache,
-    //                             { query: MeDocument },
-    //                             res,
-    //                             () => ({ me: null })
-    //                         )
-    //                     },
-    //                 }
-    //             }
-    //         })
-    //     ],
-    //     fetchOptions: {
-    //         credentials: 'include'
-    //     }
-    // })
-
     return (
         <>
             <Head>
                 <title>Alpine</title>
             </Head>
-            {/*<Provider value={client}>*/}
             <ApolloProvider client={client}>
                 <ThemeContext.Provider value={{ theme, setTheme }}>
                     <div className={theme}>
@@ -97,7 +28,6 @@ function MyApp({ Component, pageProps }: AppProps) {
                     </div>
                 </ThemeContext.Provider>
             </ApolloProvider>
-            {/*</Provider>*/}
         </>
     )
 }
